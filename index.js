18c1d37407770673d8d19ef5906e95f8aa789e7c
Julho César Moreira <julhomoreira1988@gmail.com>
2025-07-21 19:55:04 +0100

    finish Menu
    finish Menu Animation

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Menu Animation Loaded");
+const navBar = document.querySelector(".navbar")
+const allLi = document.querySelectorAll("li")
+
+
+
+allLi.forEach((li, index) => {
+
+   li.addEventListener("click", e => {
+        navBar.querySelector(".active-list").classList.remove("active-list")
+       li.classList.add("active-list")
+
+        const indicator = document.querySelector(".indicator")
+        indicator.style.transform = ` translatex(calc(${index * 92}px))`
+        // indicator.style.transform = `translateX(calc(${index * 50}px))`
+
+    })
+})
+
+
+// const myArray = ["Home", "About", "Services", "Contact"];
+
+// myArray.forEach((item, index) => {
+//     console.log(item, index)    
+// })
